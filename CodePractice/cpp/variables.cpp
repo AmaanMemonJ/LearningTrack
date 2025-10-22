c7b9a26fe110ef65ee54bef867c531c474d56b40
Amaan Memon <amaanmemon01@gmail.com>
2025-10-22 19:30:48 +0200

    Practicing variables/The basics

---
+++ CodePractice/cpp/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int x; // Declaration of an integer variable 'x'
+    x = 5; // Assignment of value 5 to variable 'x'
+    int y = 6;
+
+    int sum = x + y; // Declaration and assignment in one line
+
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+
+    return 0;
+}
