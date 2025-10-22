aa254699790a0cf7f925718c901d8415b65d7d15
Amaan Memon <amaanmemon01@gmail.com>
2025-10-22 19:20:58 +0200

    Add comments to hello.cpp

--- CodePractice/cpp/hello.cpp
+++ CodePractice/cpp/hello.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
+
+    //this is a comment
+
+
+    /*
+    This is a multi line comment*/
     std::cout << "I like pizza!" << '\n';
     std::cout << "Its really good." << '\n';
     return 0;
