d62da416424083b5339ddbf1f692dc485786cb9e
Amaan Memon <amaanmemon01@gmail.com>
2025-10-22 19:44:08 +0200

    Practice variables in variables.cpp

--- CodePractice/cpp/variables.cpp
+++ CodePractice/cpp/variables.cpp
@@ -12,5 +12,39 @@ int main() {
     std::cout << y << '\n';
     std::cout << sum << '\n';
 
+
+    int age = 21; 
+    int year = 2023; 
+    int days = 7.5; 
+
+    std::cout << days;
+
+    //double pi = 3.14;
+
+    double price = 4.99;
+    std::cout << price << '\n';
+
+    //single character
+    char grade = 'A';
+    std::cout << grade << '\n'; 
+    char initial = 'CBA';
+    std::cout << initial << '\n';
+    //overflow example
+
+
+    // boolean (true or false)
+
+    bool student = true; 
+    bool rain = false;
+    std::cout << student << '\n';
+    std::cout << rain << '\n';  
+
+    //string (Objects that represent a sequence of characters)
+    std::string name = "Amaan";
+    std::string day = "Friday";
+    std::string food = "Pizza";
+    std::string address =  "123 main st.";
+    std::cout << "hello "   << name << '\n';
+    std::cout << "you are " << age << " years old" << '\n';
     return 0;
 }
