738fedb3a363522b21c7c189586d574b307a3264
Amaan Memon <amaanmemon01@gmail.com>
2025-10-22 19:30:17 +0200

    Practicing variables/The basics

--- CodePractice/cpp/hello.cpp
+++ CodePractice/cpp/hello.cpp
@@ -6,7 +6,11 @@ int main() {
 
 
     /*
-    This is a multi line comment*/
+    This 
+    is a
+    multi
+    line 
+    comment*/
     std::cout << "I like pizza!" << '\n';
     std::cout << "Its really good." << '\n';
     return 0;
