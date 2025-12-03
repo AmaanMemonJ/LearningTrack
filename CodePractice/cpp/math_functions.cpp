b898dd0210f51bb8a724181e76c0d6f5d904fed4
Amaan Memon <amaanmemon01@gmail.com>
2025-12-03 20:28:14 +0100

    practiced some basic maths functions

---
+++ CodePractice/cpp/math_functions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include <cmath>
+
+
+int main() {
+    double x = 3;
+    double y = 4; 
+    double z; 
+
+    // z = std::max(x, y);
+
+    // z = std::min(x,y);
+
+    // z = pow(x, y); // x raised to the power of y   
+    // z = sqrt(16); // square root of x
+    // z = round(4.6); // rounds to the nearest integer
+    // z = abs(-5); // absolute value
+
+    // z = ceil(4.2); // rounds up to the nearest integer
+    z = floor(4.7); // rounds down to the nearest integer
+    std::cout << z << '\n';
+
+
+
+
+    
+    // std::cout << "The minimum value is: " << z << '\n';
+
+
+    return 0; 
+
+}
